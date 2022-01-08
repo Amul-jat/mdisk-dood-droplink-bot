8fc49d24f9199a596974c5bc44b74b3029df87f4
Amul-jat <89834548+Amul-jat@users.noreply.github.com>
2022-01-08 10:40:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -169,7 +169,7 @@ async def remove_username(new_List):
         for i in new_List:
             if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i or 'https://t.me/+' in i):
                 new_List.remove(i)
-                count-=1
+        count-=1
     return new_List
 
 async def addFooter(str):
