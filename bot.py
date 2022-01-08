58c797e17cac10dcb496232f7cbd2f2139f35149
Amul-jat <89834548+Amul-jat@users.noreply.github.com>
2022-01-08 08:54:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -161,7 +161,7 @@ async def new_Doodstream_url(urls):
 
 async def remove_username(new_List):
     for i in new_List:
-        if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i or '+' in i):
+        if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i or 'https://t.me/+' in i):
             new_List.remove(i)
     return new_List
 
